62f61c497694ba685bcd95ac068ae086f61716c3
GabrielSam23 <143095210+GabrielSam23@users.noreply.github.com>
2024-05-07 13:07:10 -0300

    Add files via upload

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
 
 router.get("/", async (req, res, next) => {
-  // Caminho para o arquivo HTML
-  const htmlPath = path.join(__dirname, "index.html");
-
-  // Renderiza o arquivo HTML usando o pacote 'ejs'
-  res.render(htmlPath);
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
 });
 
 module.exports = router;
